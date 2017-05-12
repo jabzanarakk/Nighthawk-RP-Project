fcde3967eeef661b02652dce81b388e563e0b284
Eagleone <oui_eagleone@hotmail.com>
2017-05-12 23:06:24 +0700

    ไม่ต้องการ essentialmode อีกต่อไป

--- __resource.lua
+++ __resource.lua
@@ -3,9 +3,6 @@ resource_type 'gametype' { name = 'Roleplay' }
 -- Manifest
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
--- Requiring essentialmode
-dependency 'essentialmode'
-
 -- Core General
 client_script 'client_core.lua'
 server_script 'server_core.lua'
