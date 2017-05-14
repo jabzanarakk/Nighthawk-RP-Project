b34dfdb839fc41dddc600d6569ac35b34b2adf2c
Eagleone <oui_eagleone@hotmail.com>
2017-05-15 03:59:07 +0700

    Update client_core.lua

--- client_core.lua
+++ client_core.lua
@@ -1,3 +1,14 @@
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		if NetworkIsSessionStarted() then
+			TriggerServerEvent('lsrp:dbLoad')
+			return
+		end
+	end
+end)
+
 AddEventHandler('onClientMapStart', function()
     exports.spawnmanager:setAutoSpawn(true)
     exports.spawnmanager:forceRespawn()
