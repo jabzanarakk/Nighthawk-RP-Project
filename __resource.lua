fded6199fcfd232e97777f4c98b81415040ca747
Eagleone <oui_eagleone@hotmail.com>
2017-05-17 10:55:42 +0700

    rename

--- __resource.lua
+++ __resource.lua
@@ -4,7 +4,7 @@ resource_type 'gametype' { name = 'Roleplay' }
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 -- Requiring essentialmode
-dependency 'oldessentialmode'
+dependency 'essentialmode'
 
 -- Core General
 client_script 'init_client.lua'
