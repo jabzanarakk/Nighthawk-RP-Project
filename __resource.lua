9822c621ea42dcf27e3f996efd869b90d95bb758
Eagleone <oui_eagleone@hotmail.com>
2017-05-19 10:08:39 +0700

    (Prototype) Banking

--- __resource.lua
+++ __resource.lua
@@ -6,6 +6,19 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 -- Requiring essentialmode
 dependency 'essentialmode'
 
+-- lib 
+ui_page 'html/banking/ui.html'
+files {
+	'html/banking/ui.html',
+	'html/banking/pricedown.ttf',
+	'html/banking/bank-icon.png',
+	'html/banking/logo.png',
+	'html/banking/cursor.png',
+	'html/banking/styles.css',
+	'html/banking/scripts.js',
+	'html/banking/debounce.min.js'
+}
+
 -- Core General
 client_script 'init_client.lua'
 server_script 'init_server.lua'
@@ -15,3 +28,7 @@ server_script 'config/config_master.lua'
 
 -- Function
 server_script 'function/server_function.lua'
+
+-- Core
+client_script 'core/banking/init_client.lua'
+server_script 'core/banking/init_server.lua'
--- init_client.lua
+++ init_client.lua
@@ -2,3 +2,12 @@ AddEventHandler('onClientMapStart', function()
     exports.spawnmanager:setAutoSpawn(true)
     exports.spawnmanager:forceRespawn()
 end)
+
+RegisterNetEvent("sarp:notify")
+AddEventHandler("sarp:notify", function(icon, type, sender, title, text)
+    Wait(1)
+    SetNotificationTextEntry("STRING");
+    AddTextComponentString(text);
+    SetNotificationMessage(icon, icon, true, type, sender, title, text);
+    DrawNotification(false, true);
+end)
