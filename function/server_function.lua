e22291581ec6e665bafd519adb5be124ebe4590d
Eagleone <oui_eagleone@hotmail.com>
2017-05-21 14:18:57 +0700

    Add new fucntion

--- function/server_function.lua
+++ function/server_function.lua
@@ -58,3 +58,32 @@ function upCharCheck (str)
         end
     end
 end
+
+function stringsplit(self, delimiter)
+  local a = self:Split(delimiter)
+  local t = {}
+
+  for i = 0, #a - 1 do
+     table.insert(t, a[i])
+  end
+
+  return t
+end
+
+function startswith(String,Start)
+   return string.sub(String,1,string.len(Start))==Start
+end
+
+function returnIndexesInTable(t)
+	local i = 0;
+	for _,v in pairs(t)do
+ 		i = i + 1
+	end
+	return i;
+end
+
+function debugMsg (msg)
+    if(settings.defaultSettings.debugInformation and msg)then
+        print("SARP DEBUG: " .. msg)
+    end
+end
