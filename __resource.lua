732ba305956b341afbf69e96a6a471c7b8781b6f
Eagleone <oui_eagleone@hotmail.com>
2017-05-21 14:35:38 +0700

    don't need essentialmode

--- __resource.lua
+++ __resource.lua
@@ -3,9 +3,6 @@ resource_type 'gametype' { name = 'Roleplay' }
 -- Manifest
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
--- Requiring essentialmode
-dependency 'essentialmode'
-
 -- lib 
 ui_page 'html/banking/ui.html'
 ui_page 'html/banking/ui2.html'
